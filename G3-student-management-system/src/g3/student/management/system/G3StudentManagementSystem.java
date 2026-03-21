e36fa8f76f898513ad12a350578e4e3163835660
seglenn <lambertcordial@gmail.com>
2026-03-21 23:35:34 +0800

    Initial commit - added all class files

---
+++ G3-student-management-system/src/g3/student/management/system/G3StudentManagementSystem.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package g3.student.management.system;
+
+/**
+ *
+ * @author Glenn
+ */
+public class G3StudentManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
