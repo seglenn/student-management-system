402dc7f99dc08538de8b897dc34cf20e6b29e8b6
seglenn <lambertcordial@gmail.com>
2026-05-21 10:21:23 +0800

    Update StudentManager.java

--- G3-student-management-system/src/g3/student/management/system/StudentManager.java
+++ G3-student-management-system/src/g3/student/management/system/StudentManager.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package g3.student.management.system;
+import java.util.ArrayList;
 
-/**
- *
- * @author Glenn
- */
 public class StudentManager {
-    
+
+    private ArrayList<Student> students;
+
+    public StudentManager() {
+        students = new ArrayList<>();
+    }
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+    public ArrayList<Student> getAllStudents() {
+        return students;
+    }
+    public void deleteStudent(int studentId) {
+        students.removeIf(s -> s.getStudentId() == studentId);
+    }
+    public Student findStudentById(int studentId) {
+        for (Student s : students) {
+            if (s.getStudentId() == studentId) {
+                return s;
+            }
+        }
+        return null;
+    }
 }
